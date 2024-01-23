128db13ccf3631625a8192f21f1b0951e37318c1
Pierre Frota <pierre@focomultimidia.com>
2024-01-22 21:16:09 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'simple-import-sort'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: true,
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+  },
+};
